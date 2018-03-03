fa2e8cc2e6c1ac145f9245797f519bf805796c61
harachaitanya <harachaitanya@gmail.com>
2018-03-03 14:57:07 -0500

    added kthpermutation program

---
+++ kthpermutation.py
@@ -0,0 +1,22 @@
+import random, math
+
+def generate_array(n):
+    return [random.randint(1,100) for _ in range(n)]
+
+a = generate_array(400000)
+
+def kthpermutation(arr,k):
+    output = []
+    while arr:
+        n = len(arr)
+        blocksize = math.factorial(n-1)
+        print(blocksize,k)
+        blockpos = (k-1)// blocksize
+        output.append(arr[blockpos])
+        del arr[blockpos]
+        k = k - blockpos*blocksize
+    return output
+
+#print(a)
+#print(kthpermutation(a,4))
+kthpermutation(a,80)
